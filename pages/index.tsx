28814b397363242150a6db961399923433a66155
zhouhuahu91 <Zhouhua.hu@gmail.com>
2022-08-13 20:33:36 +0200

    added portrait

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
 // NextJs imports
 import type { NextPage } from "next";
+import Image from "next/image";
 // Module imports
 import Emoji from "@/components/Emoji";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
-      {/* <h1 className="text-xl font-medium">
-        Hello I'm Zhouhua <Emoji symbol="👋" />, and I love building things on
-        the web.
-      </h1> */}
+    <div className="flex flex-col justify-center items-center mt-20">
+      <div className="text-center">
+        <h1 className="text-7xl mb-4">Zhouhua Hu</h1>
+        <h2 className="text-4xl">Front-End Developer</h2>
+      </div>
+      <div className="p-10 flex items-center justify-center rounded-full border mt-16">
+        <div className="relative w-[450px] h-[600px] overflow-hidden rounded-full">
+          <Image
+            src="/images/profile.JPG"
+            alt="Zhouhua Hu"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      </div>
     </div>
   );
 };
